82f5b0b57b0af63ec058d13f6a0a0c479e58666b
YasiTL <yasaman.most@gmail.com>
2018-11-07 14:00:02 -0600

    test getting video streap with PIL image

--- testvideostream.py
+++ testvideostream.py
@@ -1,11 +1,14 @@
 from io import BytesIO
 from time import sleep
 from picamera import PiCamera
+from PIL import Image
 
-# Create an in-memory stream
-my_stream = BytesIO()
+# Create the in-memory stream
+stream = BytesIO()
 camera = PiCamera()
 camera.start_preview()
-# Camera warm-up time
 sleep(2)
-camera.capture(my_stream, 'jpeg')
+camera.capture(stream, format='jpeg')
+# "Rewind" the stream to the beginning so we can read its content
+stream.seek(0)
+image = Image.open(stream)
