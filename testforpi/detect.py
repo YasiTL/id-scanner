ebd246a0b9cc6e0d8cdf112b21490ca1923b8bfe
YasiTL <yasaman.most@gmail.com>
2018-12-20 22:57:05 -0600

    trying to fix imshow error

--- testforpi/detect.py
+++ testforpi/detect.py
@@ -9,7 +9,7 @@ import numpy as np
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
-	help="path to the (optional) video file")
+                help="path to the (optional) video file")
 args = vars(ap.parse_args())
 
 # if the video path was not supplied, grab the reference to the
@@ -20,52 +20,53 @@ if not args.get("video", False):
 
 # otherwise, load the video
 else:
-	vs = cv2.VideoCapture(args["video"])
+    vs = cv2.VideoCapture(args["video"])
 
 # keep looping over the frames
 while True:
-	# grab the current frame and then handle if the frame is returned
-	# from either the 'VideoCapture' or 'VideoStream' object,
-	# respectively
-	frame = vs.read()
-	frame = frame[1] if args.get("video", False) else frame
+    # grab the current frame and then handle if the frame is returned
+    # from either the 'VideoCapture' or 'VideoStream' object,
+    # respectively
+    frame = vs.read()
+    frame = frame[1] if args.get("video", False) else frame
 
-	# check to see if we have reached the end of the
-	# video
-	if frame is None:
-		break
+    # check to see if we have reached the end of the
+    # video
+    if frame is None:
+        break
 
-	rectandbox = simple_detection.detectbarcode(frame)
+    rectandbox = simple_detection.detectbarcode(frame)
 
-	if rectandbox == None:
-		rect = None
-		box = None
-	else:
-		rect = rectandbox[0]
-		box = rectandbox[1]
-	
-	croppedimg = simple_detection.crop_minAreaRect(frame, rect)
+    if rectandbox == None:
+        rect = None
+        box = None
+    else:
+        rect = rectandbox[0]
+        box = rectandbox[1]
 
-	# if a barcode was found, draw a bounding box on the frame
-	if box is not None:
-		cv2.drawContours(frame, [box], -1, (0, 255, 0), 2)
+    # if a barcode was found, draw a bounding box on the frame
+    if box is not None:
+        cv2.drawContours(frame, [box], -1, (0, 255, 0), 2)
 
-	# show the frame and record if the user presses a key
-	cv2.imshow("Frame", frame)
-	cv2.imshow("Cropped", croppedimg)
-	key = cv2.waitKey(1) & 0xFF
+    if rect is not None:
+        croppedframe = simple_detection.crop_minAreaRect(frame, rect)
+        cv2.imshow("Cropped", croppedframe)
 
-	# if the 'q' key is pressed, stop the loop
-	if key == ord("q"):
-		break
+    # show the frame and record if the user presses a key
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the 'q' key is pressed, stop the loop
+    if key == ord("q"):
+        break
 
 # if we are not using a video file, stop the video file stream
 if not args.get("video", False):
-	vs.stop()
+    vs.stop()
 
 # otherwise, release the camera pointer
 else:
-	vs.release()
+    vs.release()
 
 # close all windows
 cv2.destroyAllWindows()
--- testforpi/simple_detection.py
+++ testforpi/simple_detection.py
@@ -3,69 +3,70 @@ import numpy as np
 import cv2
 import imutils
 
+
 def crop_minAreaRect(img, rect):
 
     # rotate img
     angle = rect[2]
-    rows,cols = img.shape[0], img.shape[1]
-    M = cv2.getRotationMatrix2D((cols/2,rows/2),angle,1)
-    img_rot = cv2.warpAffine(img,M,(cols,rows))
+    rows, cols = img.shape[0], img.shape[1]
+    M = cv2.getRotationMatrix2D((cols/2, rows/2), angle, 1)
+    img_rot = cv2.warpAffine(img, M, (cols, rows))
 
     # rotate bounding box
-    rect0 = (rect[0], rect[1], 0.0) 
+    rect0 = (rect[0], rect[1], 0.0)
     box = cv2.boxPoints(rect0)
-    pts = np.int0(cv2.transform(np.array([box]), M))[0]    
+    pts = np.int0(cv2.transform(np.array([box]), M))[0]
     pts[pts < 0] = 0
 
     # crop
-    img_crop = img_rot[pts[1][1]:pts[0][1], 
+    img_crop = img_rot[pts[1][1]:pts[0][1],
                        pts[1][0]:pts[2][0]]
 
     return img_crop
 
+
 def detectbarcode(image):
-	# convert the image to grayscale
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-	# compute the Scharr gradient magnitude representation of the images
-	# in both the x and y direction using OpenCV 2.4
-	ddepth = cv2.CV_32F
-	gradX = cv2.Sobel(gray, ddepth=ddepth, dx=1, dy=0, ksize=-1)
-	gradY = cv2.Sobel(gray, ddepth=ddepth, dx=0, dy=1, ksize=-1)
+    # compute the Scharr gradient magnitude representation of the images
+    # in both the x and y direction using OpenCV 2.4
+    ddepth = cv2.CV_32F
+    gradX = cv2.Sobel(gray, ddepth=ddepth, dx=1, dy=0, ksize=-1)
+    gradY = cv2.Sobel(gray, ddepth=ddepth, dx=0, dy=1, ksize=-1)
 
-	# subtract the y-gradient from the x-gradient
-	gradient = cv2.subtract(gradX, gradY)
-	gradient = cv2.convertScaleAbs(gradient)
+    # subtract the y-gradient from the x-gradient
+    gradient = cv2.subtract(gradX, gradY)
+    gradient = cv2.convertScaleAbs(gradient)
 
-	# blur and threshold the image
-	blurred = cv2.blur(gradient, (9, 9))
-	(_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
+    # blur and threshold the image
+    blurred = cv2.blur(gradient, (9, 9))
+    (_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
 
-	# construct a closing kernel and apply it to the thresholded image
-	kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
-	closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+    # construct a closing kernel and apply it to the thresholded image
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
+    closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 
-	# perform a series of erosions and dilations
-	closed = cv2.erode(closed, None, iterations=4)
-	closed = cv2.dilate(closed, None, iterations=4)
+    # perform a series of erosions and dilations
+    closed = cv2.erode(closed, None, iterations=4)
+    closed = cv2.dilate(closed, None, iterations=4)
 
-	# find the contours in the thresholded image
-	cnts = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    # find the contours in the thresholded image
+    cnts = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
-	# if no contours were found, return None
-	if len(cnts) == 0:
-		return None
+    # if no contours were found, return None
+    if len(cnts) == 0:
+        return None
 
-	# otherwise, sort the contours by area and compute the rotated
-	# bounding box of the largest contour
-	c = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
-	rect = cv2.minAreaRect(c)
-	box = cv2.boxPoints(rect)
-	box = np.int0(box)
+    # otherwise, sort the contours by area and compute the rotated
+    # bounding box of the largest contour
+    c = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
+    rect = cv2.minAreaRect(c)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
 
-
-	# return the bounding box of the barcode
-	return (rect, box)
-	# return rect
+    # return the bounding box of the barcode
+    return (rect, box)
+    # return rect
