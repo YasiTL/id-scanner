45f9828b91ab0d67f4378c765fec25915d9a9fd3
YasiTL <yasaman.most@gmail.com>
2018-12-20 23:37:36 -0600

    delete unused function

--- testforpi/simple_detection.py
+++ testforpi/simple_detection.py
@@ -3,28 +3,6 @@ import numpy as np
 import cv2
 import imutils
 
-
-def crop_minAreaRect(img, rect):
-
-    # rotate img
-    angle = rect[2]
-    rows, cols = img.shape[0], img.shape[1]
-    M = cv2.getRotationMatrix2D((cols/2, rows/2), angle, 1)
-    img_rot = cv2.warpAffine(img, M, (cols, rows))
-
-    # rotate bounding box
-    rect0 = (rect[0], rect[1], 0.0)
-    box = cv2.boxPoints(rect0)
-    pts = np.int0(cv2.transform(np.array([box]), M))[0]
-    pts[pts < 0] = 0
-
-    # crop
-    img_crop = img_rot[pts[1][1]:pts[0][1],
-                       pts[1][0]:pts[2][0]]
-
-    return img_crop
-
-
 def detectbarcode(image):
     # convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
