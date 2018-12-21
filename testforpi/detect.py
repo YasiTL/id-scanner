0466867d0ea24e684738d3836ef8a9decc9f48fd
YasiTL <yasaman.most@gmail.com>
2018-12-20 23:18:06 -0600

    try new cropping funct

--- testforpi/detect.py
+++ testforpi/detect.py
@@ -48,12 +48,37 @@ while True:
     if box is not None:
         cv2.drawContours(frame, [box], -1, (0, 255, 0), 2)
 
-    if rect is not None:
-        croppedframe = simple_detection.crop_minAreaRect(frame, rect)
-        cv2.imshow("Cropped", croppedframe)
+    W = rect[1][0]
+    H = rect[1][1]
+
+    Xs = [i[0] for i in box]
+    Ys = [i[1] for i in box]
+    x1 = min(Xs)
+    x2 = max(Xs)
+    y1 = min(Ys)
+    y2 = max(Ys)
+
+    angle = rect[2]
+    if angle < -45:
+        angle += 90
+
+    # Center of rectangle in source image
+    center = ((x1+x2)/2, (y1+y2)/2)
+    # Size of the upright rectangle bounding the rotated rectangle
+    size = (x2-x1, y2-y1)
+    M = cv2.getRotationMatrix2D((size[0]/2, size[1]/2), angle, 1.0)
+    # Cropped upright rectangle
+    cropped = cv2.getRectSubPix(img, size, center)
+    cropped = cv2.warpAffine(cropped, M, size)
+    croppedW = H if H > W else W
+    croppedH = H if H < W else W
+    # Final cropped & rotated rectangle
+    croppedRotated = cv2.getRectSubPix(
+        cropped, (int(croppedW), int(croppedH)), (size[0]/2, size[1]/2))
 
     # show the frame and record if the user presses a key
     cv2.imshow("Frame", frame)
+    cv2.imshow("CroppedRotated", croppedRotated)
     key = cv2.waitKey(1) & 0xFF
 
     # if the 'q' key is pressed, stop the loop
